724b941c1ba3d7344ec2bd30ee33f2c2c7018ce9
spbhanda <spbhanda@gmail.com>
2022-07-10 22:03:15 -0500

    final

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,10 @@
 // create the about section
-const generateAbout = (aboutText) => {
-   if (!aboutText) {
-      return "";
-   }
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
 
-   return `
+  return `
     <section class="my-3" id="about">
       <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
       <p>${aboutText}</p>
@@ -13,57 +13,60 @@ const generateAbout = (aboutText) => {
 };
 
 // create the projects section
-const generateProjects = (projectsArr) => {
-   return `
+const generateProjects = projectsArr => {
+  return `
     <section class="my-3" id="portfolio">
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
       <div class="flex-row justify-space-between">
       ${projectsArr
-         .filter(({ feature }) => feature)
-         .map(({ name, description, languages, link }) => {
-            return `
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
           <div class="col-12 mb-2 bg-dark text-light p-3">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.map((language) => language).join(",")}
+              ${languages.map(language => language).join(',')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
-         })
-         .join("")}
+        })
+        .join('')}
 
       ${projectsArr
-         .filter(({ feature }) => !feature)
-         .map(({ name, description, languages, link }) => {
-            return `
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          console.log(languages);
+          return `
           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(", ")}
+              ${languages.join(', ')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
-         })
-         .join("")}
+        })
+        .join('')}
+    
       </div>
     </section>
   `;
 };
 
 // export function to generate entire page
-module.exports = (templateData) => {
-   // destructure page data by section
-   const { projects, about, ...header } = templateData;
+module.exports = templateData => {
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
 
-   return `
+  return `
   <!DOCTYPE html>
   <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -88,7 +91,7 @@ module.exports = (templateData) => {
       ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
     </footer>
   </body>
   </html>
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,4 +1,4 @@
- const fs = require('fs');
+const fs = require('fs');
 
 // writing files
 const writeFile = fileContent => {
@@ -14,7 +14,7 @@ const writeFile = fileContent => {
         message: 'File created!'
       });
     });
-  }); 
+  });
 };
 
 // copying file
@@ -22,11 +22,7 @@ const copyFile = () => {
   return new Promise((resolve, reject) => {
     fs.copyFile('./src/style.css', './dist/style.css', err => {
       if (err) {
-        reject({
-          ok: false,
-          message: 'Something went wrong!',
-          err
-        });
+        reject(err);
         return;
       }
 
